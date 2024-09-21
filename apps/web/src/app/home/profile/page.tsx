71cd37df554affad70d3f3f7c4b88389c467966b
chigivera <ayman.benchamkha@gmail.com>
2024-09-21 19:52:46 +0000

    Revert "added profile successfully"
    This reverts commit a26597a622ac935002c5744e8bd3ce59c86c9b1b.

--- apps/web/src/app/home/profile/page.tsx
+++ apps/web/src/app/home/profile/page.tsx
@@ -3,7 +3,7 @@ import { Col, Image, Row } from "antd";
 import CommunityListItem from "@ntla9aw/ui/src/components/molecules/CommunityListItem";
 import ProfileTags from "@ntla9aw/ui/src/components/molecules/ProfileTags";
 import CommunityList from "@ntla9aw/ui/src/components/organisms/CommunityList";
-import ProfileInfo from "@ntla9aw/ui/src/components/atoms/ProfileInfo";
+import ProfileInfo from '@ntla9aw/ui/src/components/atoms/ProfileInfo'
 const communityData = [
   {
     id: 1,
@@ -56,26 +56,18 @@ export default function Profile() {
             preview={false} // Disable image preview
           />
           {/* TODO TAGS */}
-          <ProfileTags data={tagsData} title={"Interests"} />
+          <ProfileTags data={tagsData} title={"Interests"}/>
           {/* TODO COMMUNITIES */}
-          <CommunityList
-            Card={CommunityListItem}
-            data={communityData}
-            title={"Communities"}
-          />
+          <CommunityList Card={CommunityListItem} data={communityData} title={"Communities"} />
         </Col>
         <Col
           xs={24}
           sm={12}
           md={16}
           lg={18}
-          style={{
-            display: "flex",
-            alignItems: "flex-start",
-            paddingTop: "5em",
-          }}
+          style={{ display: "flex", alignItems: "flex-start",paddingTop:'5em' }}
         >
-          <ProfileInfo name={"user"} email="user@example.com" />
+         <ProfileInfo name={"user"} email="user@example.com"/>
         </Col>
       </Row>
     </>
